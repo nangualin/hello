f11fe92beff61260718ea678b3ac639386b74bb6
曾楠琳 <zengnanlin284@pingan.com.cn>
2017-09-21 14:23:02 +0800

    first commit

---
+++ src/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nangua
+ * Date: 17-9-21
+ * Time: 上午10:53
+ */
+namespace Hello;
+
+class HelloWorld
+{
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
+
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nangua
+ * Date: 17-9-21
+ * Time: 下午2:12
+ */
+require __DIR__ . '/vendor/autoload.php';
+use Hello\HelloWorld;
+echo HelloWorld::world();
